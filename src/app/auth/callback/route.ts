6b9f52591e52448934331771585737ded71540fd
Mike Liang <mlian031@uottawa.ca>
2025-04-03 22:25:33 -0400

    remove origin usage

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
-  const origin = requestUrl.origin
   
   if (code) {
     const supabase = await createClient()
